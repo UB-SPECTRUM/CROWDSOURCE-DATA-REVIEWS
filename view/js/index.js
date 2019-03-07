e46167e0424ea1b9a7b9917d318753c48b0f0103
venkat3151 <tcevenki@gmail.com>
2019-03-07 03:14:55 -0500

    Import Dataset Page

---
+++ view/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    //@naresh action dynamic childs
+    var next = 0;
+    $("#add-more").click(function(e){
+        e.preventDefault();
+         $("#question-id").append('<br><br><div class="form-group">'+
+            '<label class="col-md-2 control-label" for="action_question">Question</label>'+
+            '<div class="col-md-5">'+
+            '<input id="action_question" name="action_question" type="text" placeholder="" class="form-control input-md">'+
+            '</div></div>');  
+    });
+
+    // $("#remove-more").on("click", function() {  
+    //             $("#question-id").children().last().remove();  
+    // }); 
+
+});
