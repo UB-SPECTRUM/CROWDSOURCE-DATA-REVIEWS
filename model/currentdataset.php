95afa8bdec02970f38b665f7d9ca90c4b29807cb
venkat3151 <tcevenki@gmail.com>
2019-03-14 02:32:13 -0400

    dynamic data insert in DB

--- model/currentdataset.php
+++ model/currentdataset.php
@@ -6,12 +6,16 @@ if(isset($_POST['submit_row']))
  $password="";
  $databasename="spectrum";
  $dbh = new PDO('mysql:host=localhost;dbname=spectrum', $username, $password);
+ $dbhques = new PDO('mysql:host=localhost;dbname=spectrum', $username, $password);
  
  $datasetname=$_POST['Name'];
  $description=$_POST['Description'];
  $split=$_POST['split'];
  $data = file_get_contents($_FILES['file']['tmp_name']);
  $file_type = $_FILES['file']['type'];
+
+ $question = $_POST['question'];
+
  $stmt = $dbh->prepare("insert into tbl_current_dataset values('',?,?,?,?,?)");
  $stmt->bindParam(1,$datasetname);
  $stmt->bindParam(2,$description);
@@ -19,6 +23,16 @@ if(isset($_POST['submit_row']))
  $stmt->bindParam(4,$data);
  $stmt->bindParam(5,$split);
  $stmt->execute();
+
+ $currentID = $dbh->lastInsertId();
+
+for($i=0;$i<count($question);$i++){
+	$stmtques = $dbhques->prepare("insert into tbl_dataset_questions values(?,'',?)");
+ 	$stmtques->bindParam(1,$currentID);
+ 	$stmtques->bindParam(2,$question[$i]);
+ 	$stmtques->execute();
+}
+ 
  
 }
 ?>
--- view/js/index.js
+++ view/js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
          $("#question-id").append('<br><br><div class="form-group">'+
             '<label class="col-md-2 control-label" for="action_question">Question</label>'+
             '<div class="col-md-5">'+
-            '<input id="action_question" name="action_question" type="text" placeholder="" class="form-control input-md">'+
+            '<input id="action_question" name="question[]" type="text" placeholder="" class="form-control input-md">'+
             '</div></div>');  
     });
 
